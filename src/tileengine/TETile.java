c75d4ccd6e78d97d7f12d85aa7015c1653011723
aaravd18 <145805029+aaravd18@users.noreply.github.com>
2025-01-16 09:18:58 +0800

    Update TETile.java

--- src/tileengine/TETile.java
+++ src/tileengine/TETile.java
@@ -8,17 +8,9 @@ import edu.princeton.cs.algs4.StdDraw;
 import utils.RandomUtils;
 
 /**
- * The TETile object is used to represent a single tile in your world. A 2D array of tiles make up a
- * board, and can be drawn to the screen using the TERenderer class.
- *
+ * The TETile object is used to represent a single tile
  * All TETile objects must have a character, textcolor, and background color to be used to represent
- * the tile when drawn to the screen. You can also optionally provide a path to an image file of an
- * appropriate size (16x16) to be drawn in place of the unicode representation. If the image path
- * provided cannot be found, draw will fallback to using the provided character and color
- * representation, so you are free to use image tiles on your own computer.
- *
- * The provided TETile is immutable, i.e. none of its instance variables can change. You are welcome
- * to make your TETile class mutable, if you prefer.
+ * the tile when drawn to the screen.
  */
 
 public class TETile {
